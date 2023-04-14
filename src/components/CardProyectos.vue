e98b3f2d66bfd4d2a37cb14a7cf76a29d3133e49
Jimmy Osma <josma109@gmail.com>
2023-04-14 18:48:47 -0500

    add fix auto darkmode

--- src/components/CardProyectos.vue
+++ src/components/CardProyectos.vue
@@ -21,8 +21,7 @@ export default {
         return {
             proyectosUser
         }
-    }
-
+    } 
 }
 </script>
 
--- src/composable/DarkMode.js
+++ src/composable/DarkMode.js
@@ -2,17 +2,31 @@ import { ref, watchEffect } from 'vue';
 
 export default function useDarkMode() {
   const darkMode = ref(false);
-
+  
   const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+  
+  darkMode.value = prefiereDarkMode.matches; // detectar si el modo oscuro está activado en el navegador del usuario (true o false)
   prefiereDarkMode.addEventListener('change', () => {
     darkMode.value = prefiereDarkMode.matches;
   });
+  
   watchEffect(() => {
     document.body.classList.toggle('dark-mode', darkMode.value);
+    if (darkMode.value) {
+      localStorage.setItem('dark-mode', true); // almacenar la preferencia del usuario en localStorage
+    } else {
+      localStorage.removeItem('dark-mode');
+    }
   });
 
   function toggleDarkMode() {
-    darkMode.value = !darkMode.value;
+    darkMode.value = !darkMode.value; 
+  }
+
+  // detectar si el usuario ha establecido una preferencia manual para el modo oscuro
+  const localDarkMode = localStorage.getItem('dark-mode');
+  if (localDarkMode !== null) {
+    darkMode.value = localDarkMode === 'true';
   }
 
   return {
